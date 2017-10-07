1ec9ad1e927c41231d41801442d9400ea37dbde2
Chris Hambleton <christopher_hambleton@trimble.com>
2017-10-06 23:39:12 -0600

    added drop zone

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import { GiphyService } from '../services/giphy.service';
 import { Giphy } from '../services/giphy.model';
 import { ClipboardService } from '../services/clipboard.service';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -17,7 +16,8 @@ export class DashboardComponent implements OnInit {
   public savedGiphies: Giphy[];
   public imageCopied: boolean = false;
 
-  constructor(public GiphyService: GiphyService, public ClipboardService: ClipboardService) { }
+  constructor(public GiphyService: GiphyService, 
+              public ClipboardService: ClipboardService) { }
 
   ngOnInit() {
     this.savedGiphies = this.GiphyService.getSavedGiphies();   
@@ -53,4 +53,23 @@ export class DashboardComponent implements OnInit {
     this.GiphyService.delete(giphy);    
   }
 
+  public allowDrop(ev) {
+    //console.log("ALLOW DROP: " + ev);
+    ev.preventDefault();
+  }
+
+  public drag(ev) {
+    //console.log("DRAG: " + ev);
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+  public drop(ev) {
+    ev.preventDefault();
+    //console.log("DROP: " + ev);
+    var data = ev.dataTransfer.getData("text");
+    var newGiphy: Giphy = { caption: "New", imageUrl: data, clickCount: 0};
+    this.GiphyService.add(newGiphy);
+    //console.log("DATA: " + data);
+    //ev.target.appendChild(document.getElementById(data));
+  }
 }
