75581541ad34c4e5fe3312f8484db68a1021b3d7
Chris Hambleton <christopher_hambleton@trimble.com>
2017-10-27 21:02:06 -0600

    refactor for dashboard.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdChipsModule, MdInputModule, MdToolbarModule, MdCardModule, MdListModule, MdIconModule } from '@angular/material';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { Ng2Webstorage } from 'ng2-webstorage';
+import { MdToolbarModule } from '@angular/material';
 
+import { DashboardModule} from './dashboard/dashboard.module';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { InlineEditComponent } from './dashboard/inline-edit/inline-edit.component';
-import { InlineTagsComponent } from './dashboard/inline-tags/inline-tags.component';
-import { GiphyService } from './services/giphy.service';
-import { ClipboardService } from './services/clipboard.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    InlineEditComponent,
-    InlineTagsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,    
-    FormsModule,
-    FlexLayoutModule,
     AppRoutingModule,
-    Ng2Webstorage,
-    HttpModule,
-    MdButtonModule, MdInputModule, MdChipsModule, MdToolbarModule, MdListModule, MdIconModule, MdCardModule
+    DashboardModule,
+    MdToolbarModule
   ],
-  providers: [GiphyService, ClipboardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdButtonModule, MdChipsModule, MdInputModule, MdCardModule, MdListModule, MdIconModule } from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { DashboardComponent } from './dashboard.component';
+import { InlineEditComponent } from './inline-edit/inline-edit.component';
+import { InlineTagsComponent } from './inline-tags/inline-tags.component';
+import { GiphyService } from '../services/giphy.service';
+import { ClipboardService } from '../services/clipboard.service';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    InlineEditComponent,
+    InlineTagsComponent
+    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule, 
+    HttpModule,    
+    FlexLayoutModule,    
+    FormsModule,
+    Ng2Webstorage,
+    MdButtonModule, MdInputModule, MdChipsModule, MdListModule, MdIconModule, MdCardModule
+  ],
+  providers: [GiphyService, ClipboardService],
+})
+export class DashboardModule {}
