916ffd407dc7815f2a7b329aebc1099f2dd0663c
Chris Hambleton <christopher_hambleton@trimble.com>
2017-11-14 15:34:29 -0700

    added tags tests

--- src/app/dashboard/inline-tags/inline-tags.component.spec.ts
+++ src/app/dashboard/inline-tags/inline-tags.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { MdChipsModule, MdIconModule } from '@angular/material';
+import { MdChipsModule, MdIconModule, MdChipInputEvent } from '@angular/material';
 import { InlineTagsComponent } from '../inline-tags/inline-tags.component';
 
 describe('InlineTagsComponent', () => {
@@ -19,9 +19,29 @@ describe('InlineTagsComponent', () => {
     fixture = TestBed.createComponent(InlineTagsComponent);
     component = fixture.componentInstance;    
     fixture.detectChanges();
+    
+    component.tags = [];
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add an item', () => {
+    var event: any = {input: {}, value: "tag"};    
+    
+    component.add(event);
+    expect(component.tags[0].name).toBe(event.value);    
+  });
+
+  it('should remove an item', () => {
+    var event1: any = {input: {}, value: "tag1"};        
+    component.add(event1);
+    var event2: any = {input: {}, value: "tag2"};        
+    component.add(event2);
+    
+    component.remove(event2)
+    expect(component.tags[0].name).toBe(event1.value);    
+  });
+
 });
