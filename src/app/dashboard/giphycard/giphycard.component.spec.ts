33e1f6383e6bc999000a7d2af815dda050e69747
Chris Hambleton <christopher_hambleton@trimble.com>
2017-11-14 16:03:36 -0700

    added card tests

--- src/app/dashboard/giphycard/giphycard.component.spec.ts
+++ src/app/dashboard/giphycard/giphycard.component.spec.ts
@@ -13,7 +13,10 @@ import { ClipboardService } from '../../services/clipboard.service';
 
 describe('GiphyCardComponent', () => {
   let component: GiphyCardComponent;
+  let giphyService: GiphyService;
+  let clipboardService: ClipboardService;
   let fixture: ComponentFixture<GiphyCardComponent>;
+  var giphy: Giphy = { imageUrl: "bity", caption: "a", clickCount: 0, tags:[]};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,10 +31,65 @@ describe('GiphyCardComponent', () => {
     fixture = TestBed.createComponent(GiphyCardComponent);
     component = fixture.componentInstance;
     component.giphy = new Giphy();
+
+    giphyService = TestBed.get(GiphyService);
+    clipboardService = TestBed.get(ClipboardService);
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call svc edit() when editCaption', () => {
+    var spy = spyOn(giphyService, 'edit');    
+    component.editCaption({}, giphy);    
+    expect(spy).toHaveBeenCalled();
   });
+
+  it('should call svc editTags when onTagsUpdated', () => {
+    var spy = spyOn(giphyService, 'editTags');    
+    component.onTagsUpdated([], giphy);    
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should inc count when copyGiphyUrl ', () => {    
+    var initial = giphy.clickCount;
+    component.copyGiphyUrl(giphy);    
+    expect(giphy.clickCount).toBe(initial+1);
+  });
+
+  it('should call svc edit when copyGiphyUrl ', () => {
+    var spy = spyOn(giphyService, 'edit');    
+    component.copyGiphyUrl(giphy);    
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call cs copy when copyGiphyUrl ', () => {
+    var spy = spyOn(clipboardService, 'copy');    
+    component.copyGiphyUrl(giphy);    
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should emit copied event when copyGiphyUrl ', (done) => {
+    component.giphyCopied.subscribe(g => {
+      expect(g).toEqual(giphy);
+      done();
+    });
+            
+    component.copyGiphyUrl(giphy);        
+  });
+  
+  it('should call svc delete when deleteGiphy ', () => {
+    var spy = spyOn(giphyService, 'delete');    
+    component.deleteGiphy(giphy);    
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should emit delete event when deleteGiphy ', (done) => {
+    component.giphyDeleted.subscribe(g => {
+      expect(g).toEqual(giphy);
+      done();
+    });
+            
+    component.deleteGiphy(giphy);        
+  });
+
 });
